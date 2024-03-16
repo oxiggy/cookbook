5db4a9269ed02d97361acbba8dcbc61bea509878
Ada Void <oxiggy@oxiggy.com>
2024-03-16 21:56:16 +0400

    dev: setup gh-pages, deploy script

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
+    base: '/cookbook/',
     root: 'src',
     publicDir: 'public',
     envPrefix: 'PUBLIC_',
